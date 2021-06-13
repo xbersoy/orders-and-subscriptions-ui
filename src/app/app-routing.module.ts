d32c64a66dd7fa6f979fd35e76303f1c5663fa2c
xbersoy <burakersoy@msn.com>
2021-06-14 02:52:13 +0300

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderListComponent } from './components/order-list/order-list.component';
+import { SubscriptionListComponent } from './components/subscription-list/subscription-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'subscription', component: SubscriptionListComponent },
+  { path: 'order', component: OrderListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
